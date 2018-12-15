857c8bafab51e2bfd885f6b690ed8106f8facd95
mdolmen <mathieu.dolmen@gmail.com>
2018-12-15 16:39:01 +0100

    Day 20: add DCP's solution for #021

--- 021-words-from-string/solution.py
+++ 021-words-from-string/solution.py
@@ -12,6 +12,28 @@
 # "bedbathandbeyond", return either ['bed', 'bath', 'and', 'beyond] or ['bedbath',
 # 'and', 'beyond'].
 
+# Solution from DCP
+def find_sentence(s, dictionary):
+    starts = {0: ''}
+    for i in range(len(s) + 1):
+        new_starts = starts.copy()
+        for start_index, _ in starts.items():
+            word = s[start_index:i]
+            if word in dictionary:
+                new_starts[i] = word
+        starts = new_starts.copy()
+
+    result = []
+    current_length = len(s)
+    if current_length not in starts:
+        return None
+    while current_length > 0:
+        word = starts[current_length]
+        current_length -= len(word)
+        result.append(word)
+
+    return list(reversed(result))
+
 def extract_words(string, words):
     words.sort(key = lambda x: len(x))
     min_size = len(words[0])
