3c3db58957978e80ea839a5e88af2ff7bd122f62
mdolmen <mathieu.dolmen@gmail.com>
2018-12-15 14:49:06 +0100

    Day 20: fix #020

--- 020-min-rooms-required/solution.py
+++ 020-min-rooms-required/solution.py
@@ -5,25 +5,28 @@
 # 
 # For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 
-def minimum_rooms(lectures):
-    max_overlaps = 0
-    nb_overlaps = 0
+# Solution from DCP
+def minimum_rooms(intervals):
+    starts = sorted(start for start, end in intervals)
+    ends = sorted(end for start, end in intervals)
 
-    for i, interval1 in enumerate(lectures):
-        for j, interval2 in enumerate(lectures):
-            if j != i:
-                # end1 < start2 or start1 > end2
-                nb_overlaps += (interval1[1] < interval2[0]) or (interval1[0] > interval2[1])
-        max_overlaps = max(max_overlaps, nb_overlaps)
-
-    return max_overlaps
+    current_max = 0
+    current_overlap = 0
+    i, j = 0, 0
+    while i < len(intervals) and j < len(intervals):
+        if starts[i] < ends[j]:
+            current_overlap += 1
+            current_max = max(current_max, current_overlap)
+            i += 1
+        else:
+            current_overlap -= 1
+            j += 1
+    return current_max
 
 lectures = [(30, 75), (0, 50), (60, 150)]
 assert minimum_rooms(lectures) == 2
 
-# TODO : does not work for this test case
-lectures = [(30, 75), (0, 20), (100, 150), (30, 90)]
-print(minimum_rooms(lectures))
-#assert minimum_rooms(lectures) == 1
+lectures = [(30, 75), (0, 20), (100, 150)]
+assert minimum_rooms(lectures) == 1
 
 print("[+] All tests done.")
